0c4c14c23ce15f180b48261060c567037615296a
andersonzach02 <andersonzach02@gmail.com>
2020-08-07 18:19:19 -0400

    Got a Kanji character to appear on the screen from its character code.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-
+let word;
 
 function setup() {
+    createCanvas(400, 400);
+        
 
-
+    word = String.fromCharCode('0x30A1');
 }
 
 function draw() {
-    
+    background(0);
+
+    fill(255);
+    text(word, width/2, height/2);
 }
