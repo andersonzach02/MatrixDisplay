604f1812ace6dcb23ecb8dac1a8e0c20578b8b47
andersonzach02 <andersonzach02@gmail.com>
2020-08-07 22:28:46 -0400

    Created a class to represent a leading character in the code rain stream (it has a different color) that extends the RainCodeCharacter class.

---
+++ LeadingRainCodeCharacter.js
@@ -0,0 +1,7 @@
+class LeadingRainCodeCharacter extends RainCodeCharacter {
+
+    constructor(xPosition, yPosition) {
+        super(xPosition, yPosition);
+        this.color = color("#dbffe4");
+    }
+}
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function setup() {
     createCanvas(400, 400);
 
     // trailingCharacterColor = color("#00FF41");
-    leadingCharacterColor = color("#dbffe4");
+    // leadingCharacterColor = color("#dbffe4");
 
-    word = new RainCodeCharacter(width / 2, height / 2);
+    word = new LeadingRainCodeCharacter(width / 2, height / 2);
 
 }
 
